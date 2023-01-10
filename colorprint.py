5468645d967867fe8d62e0d647640264b50eefef
asterite <asterite@seclab.cs.msu.ru>
2023-01-10 13:15:09 +0300

    colorprint: add white() function

--- colorprint.py
+++ colorprint.py
@@ -5,6 +5,7 @@ COLOR_PRINT = os.isatty(1)
 class colors:
     OKGREEN = "\33[38;5;35m"
     FAIL = '\33[38;5;1m'
+    WHITE = '\u001B[37m'
     ENDC = '\033[0m'
 
 def green(s):
@@ -15,4 +16,9 @@ def green(s):
 def red(s):
     if COLOR_PRINT:
         return colors.FAIL + s + colors.ENDC
+    return s
+
+def white(s):
+    if COLOR_PRINT:
+        return colors.WHITE + s + colors.ENDC
     return s
